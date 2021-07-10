f1e021864831ac6c34c8a9d0264865d758836e18
eyaadh <eyaadh@gmail.com>
2021-07-10 13:56:52 +0500

    fixed the logic at checking audio files

--- player/telegram/audio_handler.py
+++ player/telegram/audio_handler.py
@@ -39,10 +39,7 @@ async def download_random_messages(count: int = 2) -> dict:
         try:
             msg = await Audio_Master.get_messages(audio_channel, msg_counter_start)
             
-            while not msg.audio:
-                msg = await Audio_Master.get_messages(audio_channel, msg_counter_start + 1)
-
-            while not msg.audio.file_name.endswith('.mp3'):
+            while (not msg.audio) and (not msg.audio.file_name.endswith('.mp3')):
                 msg = await Audio_Master.get_messages(audio_channel, msg_counter_start + 1)
 
             logging.info(f"Downloading the file from message {msg.message_id} - audio file: {msg.audio.file_name}")
